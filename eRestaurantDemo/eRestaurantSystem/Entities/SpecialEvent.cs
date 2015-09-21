5151d007a4f1a3a92d6171407012f8c88ebc5501
Matt Waggoner <mwaggoner1@studentmail.nait.ca>
2015-09-21 16:31:26 -0600

    added special event and reservation classes

---
+++ eRestaurantDemo/eRestaurantSystem/Entities/SpecialEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eRestaurantSystem.Entities
+{
+    public class SpecialEvent
+    {
+        [Key]
+        public string EventCode { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+
+
+        // Navigation Virtual Properties
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
