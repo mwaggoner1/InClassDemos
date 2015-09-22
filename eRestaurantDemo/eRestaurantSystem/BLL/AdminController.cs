b600e4b9ddfaf6f7e19dd9ebc0e0e5536cfb1f4a
Matt Waggoner <mwaggoner1@studentmail.nait.ca>
2015-09-22 16:21:41 -0600

    Setup of BLL AdminController

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel; // use for ODS access
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class AdminController
+    {
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            // the using is a transaction!
+            using (var context = new eRestaurantContext())
+            {
+                 // retrieve the data from the SpecialEvents Table
+                
+                // method syntax
+                return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+    
+                // query syntax
+            
+            }
+        }
+
+    }
+}
